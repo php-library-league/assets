49f2b759a744cdd11058ff80b6e883e868315982
90zlaya <contact@zlatanstajic.com>
2020-11-19 23:37:49 +0100

    Making assets more compact

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# assets
-This repo represents [PHP Library] official website assets based on [flatdoc]. You may find website [online].
+# Assets
+
+This repo represents [PHP Library](https://github.com/90zlaya/php-library) official website assets.
+
+## Details
+
+Based on [flatdoc](https://github.com/rstacruz/flatdoc). You may find [website online](https://php-library.zlatanstajic.com) and visit it.
+
+## Look and Feel
 
 Currently project looks like this:
 
-![Homepage of PHP Library website]
+![Homepage of PHP Library website](.extras/screenshots/php-library_-_homepage.png?clear_cache=1)
 
 But have in mind that this image might stay few commits behind.
-
-[online]: https://php-library.zlatanstajic.com
-[flatdoc]: https://github.com/rstacruz/flatdoc
-[PHP Library]: https://github.com/90zlaya/php-library
-
-[Homepage of PHP Library website]: http://link.zlatanstajic.com/images/portfolio/small/php-library.jpg?clear_cache=1
--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <?php
     $name       = 'PHP Library';
     $repository = '90zlaya/php-library';
-
     $assets_location_prefix = basename(getcwd()) === 'assets'
         ? './'
         : 'https://php-library.zlatanstajic.com/assets/';
-
-    $php_library = array(
-        'hyperlinks' => array(
-            'php_library' => array(
-                'on_github'    => array(
-                    'name' => 'View on GitHub',
-                    'url'  => 'https://github.com/' . $repository,
-                ),
-                'on_packagist' => array(
-                    'name' => 'View on Packagist',
-                    'url'  => 'https://packagist.org/packages/' . $repository,
-                ),
-                'issues'       => array(
-                    'name' => 'Issues',
-                    'url'  => 'https://github.com/' . $repository . '/issues',
-                ),
-            ),
-        ),
-    );
+    $php_library = [
+        'on_github'    => [
+            'name' => 'View on GitHub',
+            'url'  => 'https://github.com/' . $repository
+        ],
+        'on_packagist' => [
+            'name' => 'View on Packagist',
+            'url'  => 'https://packagist.org/packages/' . $repository
+        ],
+        'issues'       => [
+            'name' => 'Issues',
+            'url'  => 'https://github.com/' . $repository . '/issues'
+        ]
+    ];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,13 +30,38 @@
         <meta content="<?=$name?>" property="og:title">
         <meta content="<?=$name?> is a set of classes that contain the most useful attributes and methods that facilitate the development of Web applications" name="description">
         <!-- Flatdoc theme -->
-        <link href="<?=$assets_location_prefix?>css/style.css" rel="stylesheet">
-        <link href="<?=$assets_location_prefix?>css/theme.css" rel="stylesheet">
+        <link href="https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/theme-white/style.css" rel="stylesheet">
+        <link href="https://ricostacruz.com/flatdoc/support/theme.css" rel="stylesheet">
         <!-- Custom PHP Library style -->
-        <link href="<?=$assets_location_prefix?>css/custom.css" rel="stylesheet">
+        <style>
+          a,
+          a.big.button,
+          a.big.button:visited,
+          .menu a,
+          .menu a.active:after,
+          .menu a:visited,
+          .menu a.level-1,
+          .menu a.level-1:visited,
+          .content i,
+          .content em,
+          .content blockquote,
+          .content h4,
+          .content h5,
+          .content .small-heading,
+          .content .button + em,
+          .header h1,
+          .header li a,
+          .header h1 a,
+          .header h1 a:visited,
+          body:not(.big-h3) .content h3 { color: #08A5CD; }
+          .button { border: solid 2px #08A5CD; color: #fff; }
+          .button:visited { background: #08A5CD; }
+          a.big.button { border: solid 3px #08A5CD; }
+          .content ul > li:before { border: solid 1px #08A5CD; }
+          .title-image { background-image: url('img/background.jpg'); }
+        </style>
     </head>
     <body role="flatdoc" class="big-h3 large-brief">
-        <!-- TITLE -->
         <div class="title-area title-card title-image">
             <div class="in">
                 <div class="headline">
@@ -51,17 +70,16 @@
                 </div>
             </div>
         </div>
-        <!-- HEADER -->
         <div class="header">
             <div class="left">
                 <h1><?=$name?></h1>
                 <ul>
                     <?php
-                        foreach ($php_library['hyperlinks']['php_library'] as $link)
+                        foreach ($php_library as $link)
                         {
                             echo '<li><a href="';
                             echo $link['url'];
-                            echo '" target="_blank">';
+                            echo '" target="_blank" rel="noopener">';
                             echo $link['name'];
                             echo '</a></li>';
                         }
@@ -69,29 +87,28 @@
                 </ul>
             </div>
         </div>
-        <!-- CONTENT -->
         <div class="content-root">
             <div class="menubar">
                 <div class="section">
-                    <a class="big button" href="<?=$php_library['hyperlinks']['php_library']['on_github']['url'] . '/releases/latest'?>" target="_blank">Download</a>
+                    <a
+                        class="big button"
+                        href="<?=$php_library['on_github']['url'] . '/releases/latest'?>"
+                        target="_blank"
+                        rel="noopener"
+                    >Download</a>
                 </div>
                 <div class="menu section" role="flatdoc-menu"></div>
             </div>
             <div role="flatdoc-content" class="content"></div>
         </div>
         <!-- jQuery Load -->
-        <script src="<?=$assets_location_prefix?>js/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <!-- Flatdoc -->
-        <script src="<?=$assets_location_prefix?>js/legacy.js"></script>
-        <script src="<?=$assets_location_prefix?>js/flatdoc.js"></script>
-        <!-- Flatdoc script -->
-        <script src="<?=$assets_location_prefix?>js/script.js"></script>
-        <script src="<?=$assets_location_prefix?>js/theme.js"></script>
+        <script src="https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/legacy.js"></script>
+        <script src="https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/flatdoc.js"></script>
+        <script src="https://cdn.rawgit.com/rstacruz/flatdoc/v0.9.0/theme-white/script.js "></script>
+        <script src="https://ricostacruz.com/flatdoc/support/theme.js"></script>
         <!-- Initializer -->
-        <script>
-            Flatdoc.run({
-                fetcher: Flatdoc.github("<?=$repository?>")
-            });
-        </script>
+        <script>Flatdoc.run({ fetcher: Flatdoc.github("<?=$repository?>") });</script>
     </body>
 </html>
